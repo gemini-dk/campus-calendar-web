202d6ab5f6d4a3888358034725cbc789c18ba7ed
D.Kawamura <86137970+gemini-dk@users.noreply.github.com>
2025-10-20 02:00:43 +0900

    Add public calendar pages

---
+++ src/app/public/calendar/page.tsx
@@ -0,0 +1,7 @@
+import PublicCalendarLanding from "./_components/PublicCalendarLanding";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  return <PublicCalendarLanding />;
+}
